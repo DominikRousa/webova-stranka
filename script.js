5754f8d5a2eca4e232212b7067d66afcba2ebdba
DominikRousa <145441116+DominikRousa@users.noreply.github.com>
2024-03-24 19:10:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -37,24 +37,29 @@ window.onclick = function(event) {
   }
 }
 
+function darkmode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
 var database = [
-    "stefan.g1234",
-    "dominik.heslo",
-    "petr.c1234",
-    "jachym.d1234",
+  "stefan.g1234",
+  "dominik.heslo",
+  "petr.c1234",
+  "jachym.d1234",
 ]
 
 function login() {
-    var username = document.getElementById("username").value
-    var password = document.getElementById("password").value
-    var loginData = username + "." + password
+  var username = document.getElementById("username").value
+  var password = document.getElementById("password").value
+  var loginData = username + "." + password
 
-    for (let i = 0; i < database.length; i++) {
-        if(loginData == database[i]) {
-            console.log("Login sucessful")
-            break;
-        } else if(i == database.length - 1){
-            console.log("Incorrect input")
-        }
+  for (let i = 0; i < database.length; i++) {
+    if(loginData == database[i]) {
+        console.log("Login sucessful")
+        break;
+    } else if(i == database.length - 1){
+        console.log("Incorrect input")
     }
+  }
 }
